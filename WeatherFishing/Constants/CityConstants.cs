2efb53a400c1e8732adae4592256d27235c8db4d
Rusislav <Rusislav.Vulkov@mail.bg>
2024-02-23 10:55:57 +0200

    Adding documentation and structuring to Program.cs

---
+++ WeatherFishing/Constants/CityConstants.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatheFishing.Constants
+{
+    internal class CityConstants
+    {
+        public static readonly string UrlCurrentWeatherData = "http://www.weather.bg/index.php?koiFail=tekushti&lng=0";
+        public static readonly string UrlWeatherDataFor3Day = "http://www.meteo.bg/bg/forecast/gradove";
+        public static readonly string UrlRiverWaterLeve = "http://www.meteo.bg/bg/rekiTablitsa";
+
+        public static readonly string StartPointForDatesInWeather = "Град";
+
+        public static readonly string[] CitiesForCurrentWeather = {
+        "Монтана",
+        "Плевен",
+        "Пловдив - Тракия",
+        "Пазарджик",
+        "Кърджали"
+        };
+
+        public static readonly string[] CitiesForFor3DaysWeather =
+        {
+         "Монтана",
+        "Плевен",
+        "Пловдив",      
+        "Кърджали"
+        };
+
+        public static readonly string[] RiversWatherLevelХМС =
+        {       
+            "71650",
+            "71700",
+            "71800",
+            "72700",
+            "72850",
+            "73750",
+            "73850",
+            "16800",
+            "16850",
+            "18650",
+            "18700",
+            "18800",
+            "74650",           
+            "61650",
+            "61700",
+            "52800",
+            "52850",
+            "51650",
+            "51750",
+            "51800 ",
+            "51880 ",           
+        };
+      
+    }
+}
+
---
+++ WeatherFishing/Program.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Text;
+using WeatheFishing.Services;
+using WeatheFishing.Contracts;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using WeatheFishing.Constants;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WeatheFishing
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+           
+                // Setup dependencie
+                IServiceProvider serviceProvider = SetupDependencies();
+
+                // Retrieve necessary services
+                var logger = serviceProvider.GetService<ILogger<WeatherService>>();
+                var htmlWeb = serviceProvider.GetService<HtmlWeb>();
+                var weatherService = serviceProvider.GetService<IWeatherService>();
+
+                // Fetch data asynchronously
+                var currentWeatherData = await weatherService.GetCurrentWeatherDataAsync(CityConstants.UrlCurrentWeatherData);
+                var weatherDataFor3Days = await weatherService.GetWeatherDataFor3DaysAsync(CityConstants.UrlWeatherDataFor3Day);
+                var riverWaterLevel = await weatherService.GetRiverWatherLevelAsync(CityConstants.UrlRiverWaterLeve);
+
+            // Display results
+            DisplayData("Current Weather", currentWeatherData);
+            DisplayData("Weather in the Next 3 Days", weatherDataFor3Days);
+            DisplayData("River Water Level", riverWaterLevel);
+     
+           
+
+            static IServiceProvider SetupDependencies()
+            {
+                return new ServiceCollection()
+                    .AddLogging(builder =>
+                    {
+                        builder.AddConsole(); // Adds the console logger
+                    })                   
+                    .AddSingleton<HtmlWeb>()
+                    .AddScoped<IWeatherService, WeatherService>()
+                    .BuildServiceProvider();
+            }
+
+
+            static void DisplayData(string title, List<string> data)
+            {
+
+                Console.OutputEncoding = Encoding.UTF8;
+
+                if (data != null)
+                {
+                    Console.WriteLine($"{title} \n");
+                    Console.WriteLine(string.Join(Environment.NewLine, data));
+                    Console.WriteLine(Environment.NewLine);
+                }
+            }
+        }
+
+    }
+}
